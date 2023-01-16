aa2e08c74261d257fa5150b18ae00e12c85c1c26
Nando PL <118541862+nandopiles@users.noreply.github.com>
2023-01-16 14:20:32 +0100

    Ramonix Acabado

--- 2T/Ramonix/src/Atac.java
+++ 2T/Ramonix/src/Atac.java
@@ -2,16 +2,25 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
-import java.sql.SQLOutput;
 
 public class Atac implements Comu {
+
+    public static String messageRamonix() {
+        return "d8888b.  .d8b.  .88b  d88.  .d88b.  d8b   db d888888b db    db \n" +
+                "88  `8D d8' `8b 88'YbdP`88 .8P  Y8. 888o  88   `88'   `8b  d8' \n" +
+                "88oobY' 88ooo88 88  88  88 88    88 88V8o 88    88     `8bd8'  \n" +
+                "88`8b   88~~~88 88  88  88 88    88 88 V8o88    88     .dPYb.  \n" +
+                "88 `88. 88   88 88  88  88 `8b  d8' 88  V888   .88.   .8P  Y8. \n" +
+                "88   YD YP   YP YP  YP  YP  `Y88P'  VP   V8P Y888888P YP    YP ";
+    }
+
     public static String messageTangoDown() {
-        return "d8888b.  .d8b.  .88b  d88.  .d88b.  d8b   db d888888b db    db d888888b  .d8b.  d8b   db  d888b   .d88b.  d8888b.  .d88b.  db   d8b   db d8b   db \n" +
-                "88  `8D d8' `8b 88'YbdP`88 .8P  Y8. 888o  88   `88'   `8b  d8' `~~88~~' d8' `8b 888o  88 88' Y8b .8P  Y8. 88  `8D .8P  Y8. 88   I8I   88 888o  88 \n" +
-                "88oobY' 88ooo88 88  88  88 88    88 88V8o 88    88     `8bd8'     88    88ooo88 88V8o 88 88      88    88 88   88 88    88 88   I8I   88 88V8o 88 \n" +
-                "88`8b   88~~~88 88  88  88 88    88 88 V8o88    88     .dPYb.     88    88~~~88 88 V8o88 88  ooo 88    88 88   88 88    88 Y8   I8I   88 88 V8o88 \n" +
-                "88 `88. 88   88 88  88  88 `8b  d8' 88  V888   .88.   .8P  Y8.    88    88   88 88  V888 88. ~8~ `8b  d8' 88  .8D `8b  d8' `8b d8'8b d8' 88  V888 \n" +
-                "88   YD YP   YP YP  YP  YP  `Y88P'  VP   V8P Y888888P YP    YP    YP    YP   YP VP   V8P  Y888P   `Y88P'  Y8888D'  `Y88P'   `8b8' `8d8'  VP   V8P ";
+        return "d888888b  .d8b.  d8b   db  d888b   .d88b.  d8888b.  .d88b.  db   d8b   db d8b   db \n" +
+                "`~~88~~' d8' `8b 888o  88 88' Y8b .8P  Y8. 88  `8D .8P  Y8. 88   I8I   88 888o  88 \n" +
+                "   88    88ooo88 88V8o 88 88      88    88 88   88 88    88 88   I8I   88 88V8o 88 \n" +
+                "   88    88~~~88 88 V8o88 88  ooo 88    88 88   88 88    88 Y8   I8I   88 88 V8o88 \n" +
+                "   88    88   88 88  V888 88. ~8~ `8b  d8' 88  .8D `8b  d8' `8b d8'8b d8' 88  V888 \n" +
+                "   YP    YP   YP VP   V8P  Y888P   `Y88P'  Y8888D'  `Y88P'   `8b8' `8d8'  VP   V8P ";
     }
 
     /**
@@ -67,7 +76,7 @@ public class Atac implements Comu {
         }
 
         if (isDestroyed)
-            System.out.println(ANSI_BLUE + messageTangoDown() + ANSI_RESET);
+            System.out.println(ANSI_BLUE + messageRamonix() + "\n" + messageTangoDown() + ANSI_RESET);
 
         return isDestroyed;
     }
